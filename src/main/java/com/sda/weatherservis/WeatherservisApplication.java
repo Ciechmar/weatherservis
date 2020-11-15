339e5c8406e686ef552685a93ac1d2588565c7a4
Ciechmar <Ciechmar@o2.pl>
2020-11-15 11:16:10 +0100

    Initial commit

---
+++ src/main/java/com/sda/weatherservis/WeatherservisApplication.java
@@ -0,0 +1,13 @@
+package com.sda.weatherservis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherservisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherservisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/weatherservis/WeatherservisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.weatherservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherservisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
