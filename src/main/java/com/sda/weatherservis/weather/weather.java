f1bf442c270920e41c52ce71e9b93abf8c63db95
Ciechmar <Ciechmar@o2.pl>
2020-11-29 18:56:20 +0100

    weather init commit

---
+++ src/main/java/com/sda/weatherservis/weather/weather.java
@@ -0,0 +1,30 @@
+package com.sda.weatherservis.weather;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class weather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    Double temp;
+    Double pressure;
+    Double humidity; //wilogtność
+    Double windSpeed;
+    Double windDirection;
+
+
+}
